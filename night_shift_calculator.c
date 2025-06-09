c91698fd315a0e650ff64428784ba9b9b3db488e
Herick Vinicius <herick.gustavo@gmail.com>
2025-06-09 17:53:55 -0400

    add | wip: try some strategies to solve the problem. Not working yet.

--- night_shift_calculator.c
+++ night_shift_calculator.c
@@ -1,24 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int calculateOverlapping(shiftStart, shiftEnd, intervalStart, intervalEnd) {
-	int adjustedSE = 0;
-	int adjustedIE = 0;
+int calculateNightShift(int shiftStart, int shiftEnd, int intervalStart, int intervalEnd) {
+	// int adjustedSE = shiftEnd + 1440;
+	int adjustedIE = intervalEnd + 1440;
 	int minute = 0;
+	int limit = 0;
+	int timeWorked = 0;
 
-	minute = shiftStart;
+	// if (shiftEnd < shiftStart) {
+	// 	adjustedSE += 1440;	// Add one day in minutes.
+	// }
+	// if (intervalEnd < intervalStart) {
+	// 	adjustedIE += 1440; // Add one day in minutes.
+	// }
 
-	for (minute = shiftStart; minute < limit; minute++) {
-		if (minute < 1440) {
-			if (shiftEnd < shiftStart) {
-				adjustedSE += 1440;	// Add one day in minutes.
-			}
-			if (intervalEnd < intervalStart) {
-				adjustedIE += 1440; // Add one day in minutes.
-			}
-			// Calculate overlap
-		}
+	if (shiftStart <= intervalStart) {
+		minute = intervalStart;
+	} else {
+		minute = shiftStart;
 	}
+	limit = adjustedIE;
+
+
+	for (int i = shiftStart; i < limit; i++) {
+		if (minute < 1440) {
+			if(minute <= adjustedIE) {
+				timeWorked++;
+			}
+		} else {
+			minute = minute % 1440;
+			if(minute <= intervalEnd) {
+				timeWorked++;
+			}
+		}
+		minute++;
+	}
+	return timeWorked;
 }
 
 int main(void) {
@@ -26,6 +44,7 @@ int main(void) {
 	int shiftEnd = 0;
 	int intervalStart = 0;
 	int intervalEnd = 0;
+	// int timeWorked = 0;
 
 	printf("Inicio da Jornada: ");
 	scanf("%d", &shiftStart);
@@ -37,5 +56,9 @@ int main(void) {
 	printf("Fim da Intervalo: ");
 	scanf("%d", &intervalEnd);
 
-	timeWorked = calculateOverlapping(shiftStart, shiftEnd, intervalStart, intervalEnd);
+	int timeWorked = calculateNightShift(shiftStart, shiftEnd, intervalStart, intervalEnd);
+
+	printf('Tempo noturno trabalhado: %d\n', timeWorked);
+
+	return 0;
 }
